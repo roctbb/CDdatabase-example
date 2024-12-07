9f1e69e6f199fa9beabef87f1d540575dd52b1fd
roctbb <roctbb@gmail.com>
2024-12-08 00:30:24 +0300

    Add albums

--- app.py
+++ app.py
@@ -10,21 +10,39 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-# добавили description
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=True)
 
-# добавили description
+
+# добавили модель для альбома
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id', ondelete='CASCADE'), nullable=False)
+
+    artist = db.relationship('Artist', backref=db.backref('albums'))
+
+
+# презентер альбома
+def present_album(album):
+    return {
+        'id': album.id,
+        'name': album.name,
+        'description': album.description,
+    }
+
+
 def present_artist(artist):
     return {
         'id': artist.id,
         'name': artist.name,
-        'description': artist.description
+        'description': artist.description,
     }
 
-# не меняется
+
 @app.route('/api/artists', methods=['GET'])
 def get_all_artists():
     artists = Artist.query.all()
@@ -32,7 +50,6 @@ def get_all_artists():
     return jsonify(artist_descriptions)
 
 
-# не меняется
 @app.route('/api/artists/<int:id>', methods=['GET'])
 def get_artist_by_id(id):
     artist = Artist.query.filter_by(id=id).first()
@@ -43,22 +60,18 @@ def get_artist_by_id(id):
     return jsonify(present_artist(artist))
 
 
-# добавляем указание описания
 @app.route('/api/artists', methods=['POST'])
 def add_artist():
     data = request.get_json()
 
     name = data.get('name')
-    # получаем описание
     description = data.get('description')
 
-    # описание не проверяем - оно может быть и пустым
     if not name:
         return jsonify({'reason': 'Missing name'}), 400
     if Artist.query.filter_by(name=name).first():
         return jsonify({'reason': 'Artist already exists'}), 400
 
-    # добавляем описание к новому исполнителю
     artist = Artist(name=name, description=description)
     db.session.add(artist)
     db.session.commit()
@@ -66,7 +79,6 @@ def add_artist():
     return jsonify(present_artist(artist))
 
 
-# добавляем возможность поменять описание
 @app.route('/api/artists/<int:id>', methods=['PATCH'])
 def update_artist(id):
     data = request.get_json()
@@ -75,9 +87,6 @@ def update_artist(id):
     if not artist:
         return jsonify({'reason': 'Artist not found'}), 404
 
-    # логика PATCH подразумевает, что можно передавть не все свойства, а только те, что нужно поменять.
-    # для изменения объекта целиком используется метод PUT
-    # если передали имя - проводим проверки для имени
     if 'name' in data:
         name = data.get('name')
 
@@ -89,18 +98,15 @@ def update_artist(id):
 
         artist.name = name
 
-    # если передали любое описание - меняем описание
     if 'description' in data:
         description = data.get('description')
         artist.description = description
 
     db.session.commit()
 
-    # возвращаем описание артиста с изменениями
     return jsonify(present_artist(artist))
 
 
-# не меняется
 @app.route('/api/artists/<int:id>', methods=['DELETE'])
 def delete_artist(id):
     artist = Artist.query.filter_by(id=id).first()
@@ -114,6 +120,90 @@ def delete_artist(id):
     return jsonify({'success': True})
 
 
+# получаем список альбомов исполнителя
+@app.route('/api/artists/<int:id>/albums', methods=['GET'])
+def get_artist_albums(id):
+    artist = Artist.query.filter_by(id=id).first()
+    if not artist:
+        return jsonify({'reason': 'Artist not found'}), 404
+
+    return jsonify([present_album(album) for album in artist.albums])
+
+
+# создаем альбом для исполнителя
+@app.route('/api/artists/<int:id>/albums', methods=['POST'])
+def add_album(id):
+    artist = Artist.query.filter_by(id=id).first()
+    if not artist:
+        return jsonify({'reason': 'Artist not found'}), 404
+
+    data = request.get_json()
+    name = data.get('name')
+    description = data.get('description')
+
+    if not name or not description:
+        return jsonify({'reason': 'Missing name or description'}), 400
+
+    # не забываем указать id исполнителя
+    album = Album(name=name, description=description, artist_id=id)
+    db.session.add(album)
+    db.session.commit()
+
+    return jsonify(present_album(album))
+
+
+# обновление данных альбома
+@app.route('/api/artists/<int:id>/albums/<int:album_id>', methods=['PATCH'])
+def update_album(id, album_id):
+    artist = Artist.query.filter_by(id=id).first()
+    if not artist:
+        return jsonify({'reason': 'Artist not found'}), 404
+
+    album = Album.query.filter_by(id=album_id).first()
+    if not album or album.artist_id != id:
+        return jsonify({'reason': 'Album not found'}), 404
+
+    data = request.get_json()
+
+    if 'name' in data:
+        name = data.get('name')
+
+        if not name:
+            return jsonify({'reason': 'Missing name'}), 400
+
+        album.name = name
+
+    if 'description' in data:
+        description = data.get('description')
+
+        if not description:
+            return jsonify({'reason': 'Missing description'}), 400
+
+        album.description = description
+
+    db.session.commit()
+
+    return jsonify(present_album(album))
+
+    # удаление альбома
+
+
+@app.route('/api/artists/<int:id>/albums/<int:album_id>', methods=['DELETE'])
+def delete_album(id, album_id):
+    artist = Artist.query.filter_by(id=id).first()
+    if not artist:
+        return jsonify({'reason': 'Artist not found'}), 404
+
+    album = Album.query.filter_by(id=album_id).first()
+    if not album or album.artist_id != id:
+        return jsonify({'reason': 'Album not found'}), 404
+
+    db.session.delete(album)
+    db.session.commit()
+
+    return jsonify({'success': True})
+
+
 if __name__ == '__main__':
     # запускаем сервер
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
--- tests/artists_api.py
+++ tests/artists_api.py
@@ -1,6 +1,6 @@
 import requests
 
-API_URL = 'http://127.0.0.1:5000/api/'
+API_URL = 'http://127.0.0.1:8000/api/'
 
 
 def create_artist_request(name, description=None):
@@ -39,6 +39,24 @@ def update_artist_request(artist_id, name=None, description=None):
     )
 
 
+def create_album_request(name, description, artist_id):
+    return requests.post(
+        API_URL + 'artists/' + str(artist_id) + '/albums',
+        json={'name': name, 'description': description}
+    )
+
+
+def get_albums_request(artist_id):
+    return requests.get(
+        API_URL + 'artists/' + str(artist_id) + '/albums'
+    )
+
+def delete_album_request(artist_id, album_id):
+    return requests.delete(
+        API_URL + 'artists/' + str(artist_id) + '/albums/' + str(album_id)
+    )
+
+
 def run_artists_api_tests():
     # проверяем создание артистов
     artist_response1 = create_artist_request('Test Artist1', 'Description1')
@@ -67,6 +85,31 @@ def run_artists_api_tests():
     artist_response = update_artist_request(artist_id1, 'Test Artist1', 'Updated Description1')
     assert artist_response.status_code == 200
     print("Update artist tests passed!")
+
+    # проверяем возможность добавления и удаления альбомов
+    album_response = create_album_request('Test Album1', 'Description1', artist_id1)
+    assert album_response.status_code == 200
+
+    album_response = create_album_request('Test Album2', 'Description2', artist_id1)
+    assert album_response.status_code == 200
+
+    album_response = get_albums_request(artist_id1)
+    assert album_response.status_code == 200
+    assert len(album_response.json()) == 2
+
+    album_id1 = album_response.json()[0].get('id')
+    album_id2 = album_response.json()[1].get('id')
+
+    album_response = delete_album_request(artist_id1, album_id1)
+    assert album_response.status_code == 200
+    album_response = delete_album_request(artist_id1, album_id2)
+    assert album_response.status_code == 200
+
+    album_response = get_albums_request(artist_id1)
+    assert album_response.status_code == 200
+    assert len(album_response.json()) == 0
+
+    print("Album creation tests passed!")
 
     # проверяем, что нельзя задать артисту уже использованное имя при обновлении
     artist_response = update_artist_request(artist_id1, 'Test Artist2')
